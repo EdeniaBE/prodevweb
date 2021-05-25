73f5c3572d5a31aeced603a0ebb3ee0e04cfa5a6
Patrice Verstichel <patrice.verstichel@gmail.com>
2021-05-25 21:36:01 +0200

    DB seed

--- tests/ScriptcreationBD/projweb.sql
+++ tests/ScriptcreationBD/projweb.sql
@@ -3,8 +3,8 @@ SET time_zone = "+00:00";
 
 -- Création de la BD projdevWeb si la DB n'existe pas
 
-CREATE DATABASE IF NOT EXISTS `projdevWeb` DEFAULT CHARACTER SET utf8;
-USE `projdevWeb`;
+CREATE DATABASE IF NOT EXISTS `projdevWebtest` DEFAULT CHARACTER SET utf8;
+USE `projdevWebtest`;
 -- --------------------------------------------------------
 -- Création de la table articles
 DROP TABLE IF EXISTS `articles`;
@@ -76,7 +76,7 @@ CREATE TABLE IF NOT EXISTS `chat` (
 chat_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 id_membre INT UNSIGNED NOT NULL,
 lignes VARCHAR(255) NOT NULL
-) ENGINE-InnoDB DEFAULT CHARSET-utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- création de la table logs
 DROP TABLE IF EXISTS `logs`;
@@ -106,48 +106,35 @@ ALTER TABLE `commentaires`
   ADD CONSTRAINT FOREIGN KEY (`id_blog`) REFERENCES `blog` (`blog_id`),
   ADD CONSTRAINT FOREIGN KEY (`id_membre`) REFERENCES `profiles` (`profile_id`);
 
-
--- INSERT INTO Biens (nom_bien,commune,prix,superficie,genre,deleted) values
---		('Beaulieu','Audergem',500,110,'Appartement',FALSE),
---		('Repos','Uccle',260,105,'Appartement',FALSE),
---		('Palace','Woluwe Saint Lambert',450,80,'Appartement',FALSE),
---		('Crocus','Laeken',90,60,'Appartement',FALSE),
---		('Milkyway','Zaventem',150,95,'Maison',FALSE),
---		('Joli regard','Evere',120,60,'Appartement',FALSE),
---		('Ma campagne','Dilbeek',250,140,'Maison',FALSE),
---		('Margueritte','Saint Gilles',180,90,'Maison',FALSE),
---		('Rose','Schaerbeek',160,90,'Appartement',FALSE),
---		('Fougères','Saint Joos Ten Node',80,150,'Maison',FALSE),
---		('Coquelicots','Forest',230,120,'Maison',FALSE);
---
+-- Remplissage de la table profiles
+-- --------------------------------
+INSERT INTO profiles (nom,prenom,adresse,cp,motpasse,typeutilisateur,horodatage,indesirable) values
+		('Verstichel','Patrice','Broekstraat 98',1700,'engine',0,'2021-05-11 20:30:23',0),
+    ('Julien','Anne-Gaëlle','rue de la montagne 18',1160,'engine',0,'2021-05-12 12:15:10',0),
+    ('Brunet','Harmonie','rue de la station 486',1082,'client',1,'2021-05-20 10:10:10',0),
+    ('Nicollier','Patricia','place du mirroir 87',1090,'client',1,'2021-05-20 10:10:10',0),
+    ('Gallois','Hélène','rue longue 192',1040,'client',1,'2021-05-20 10:10:10',0);
+    
 -- --------------------------------------------------------
 
+INSERT INTO blog (id_membre,titre,corps,horodatage) values
+    (1,'Mon premier post','Ceci est mon premier post','2021-05-13 18:00:40'),
+    (1,'Mon deuxième post','Ceci est mon deuxième post','2021-05-13 18:05:40'),
+    (2,'Enfin le blog fonctionne','pas si évident que ca','2021-05-25 20:22:20'),
+    (2,'On garde le cap','Notre peuple vaincra','2021-05-25 21:00:00');
 
--- INSERT INTO Locataires (id_bien,nom,prénom) values
---		(1,'Dubois','Mickael'),
---                (2,'Thibodeau','Max'),
---                (3,'Dior','Baptiste'),
---                (4,'Emilien','Poussin'),
---                (5,'Brunet','Ariane'),
---                (6,'Abadie','Roselyne'),
---                (7,'Carpentier','Anita'),
---               (8,'Sharpe','Eloïse'),
---                (9,'Laurens','Colette'),
---                (10,'Lopez','Dolorès'),
---                (11,'Fouché','Mégane');
---
--- --------------------------------------------------------
+INSERT INTO commentaires (id_blog,id_membre,commentaire) values
+    (1,2,'Nice'),
+    (1,2,'Hé ben dis donc'),
+    (2,1,'ça marche !');
 
--- INSERT INTO proprietaires (id_bien,nom,prenom,domicile,annee_naissance,deleted) values
---                (10,'Brunet','Harmonie','Berchem-Sainte-Agathe',1952,FALSE),
---                (2,'Nicollier','Patricia','Jette',1959,FALSE),
---                (7,'Charrier','Bérénice','Etterbeek',1950,FALSE),
---                (8,'Gallois','Héléne','Ganshoren',1945,FALSE),
---                (6,'Rodier','Rebecca','Koekelberg',1931,FALSE),
---                (9,'Duval','Eve','Ixelles',1980,FALSE),
---                (1,'Chabert','Alexandre','Bruxelles',1979,FALSE),
---                (3,'Simon','Nathanaël','Watermael-Boitsfort',1986,FALSE),
---                (4,'Benett','Isaïe','Molenbeek-Saint-Jean',1987,FALSE),
---                (5,'Bechard','Jean-Marc','Woluwe-Saint-Pierre',1977,FALSE),
---                (11,'Brosseau','Grégory','Anderlecht',1964,FALSE),
---                (12,'Couturier','Yvon','Uccle',1978,FALSE);
+INSERT INTO articles (nom,categorie,prix,dispo,stock) values
+    ('ordinateur lowcost','informatique',350,1,2),
+    ('ordinateur gaming','informatique',700,1,2),
+    ('ordinateur portable','informatique',1200,1,2),
+    ('Le monde merveilleux de PHP','livre',25,1,2),
+    ('PDO pour les null','livre',35,1,2),
+    ('Informatique quand tu nous plait','livre',12,1,2),
+    ('Walkman','hi-fi',57,1,2),
+    ('MP3 a gogo','hi-fi',58,1,2),
+    ('soundbar','hi-fi',120,1,2);
